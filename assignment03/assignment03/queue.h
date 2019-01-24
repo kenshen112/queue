341c9ba9a1ea9bbe54f8c02b1daa08cfc8c5759e
Kenyon Bunker <kjbunker817@gmail.com>
2019-01-23 19:13:32 -0700

    Added basic project structure

---
+++ assignment03/assignment03/queue.h
@@ -0,0 +1,25 @@
+#pragma once
+/***********************************************************************
+* Header:
+*    QUEUE
+* Summary:
+*    A custom Queue class. First in first out
+* Author
+*    Kenyon Bunker and Tim O'Barr
+************************************************************************/
+#ifndef QUEUE_H
+#define QUEUE_H
+namespace custom {
+template <class T> class stack {
+private:
+   T *data;
+   int numPush;
+   int numPop;
+   int numCapacity;
+public:
+
+};
+
+
+}
+#endif /* STACK_H */
