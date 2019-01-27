44f46c8c646fb4e7b16bba5f31888a95b7705af4
Kenyon Bunker <kjbunker817@gmail.com>
2019-01-26 18:04:24 -0700

    edit out error code

--- assignment03/assignment03/stock.cpp
+++ assignment03/assignment03/stock.cpp
@@ -70,9 +70,9 @@ void stock::stocksBuySell()
 
 float stock::calcProfitLoss()
 { 
-   profitLoss = (sellPrice - purchasePrice) * numShares;
+   //profitLoss = (sellPrice - purchasePrice) * numShares;
 
-   return this->profitLoss += profitLoss;
+   //return this->profitLoss += profitLoss;
 }
 
 void stock::display()
